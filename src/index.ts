90963d47baf7c87dcb0e881d75af7c225d350be5
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2025-12-15 12:14:36 -0500

    feat: agregado prisma para conectarlo con la base de datos Neon

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/usuarios", async () => {
+    const usuarios = await prisma.usuarios.findMany();
+    return usuarios;
+  })
+  .post(
+    "/usuarios",
+    async ({ body }) => {
+      const nuevoUsuario = await prisma.usuarios.create({
+        data: body,
+      });
+      return nuevoUsuario;
+    },
+    {
+      body: t.Object({
+        email: t.String(),
+        password_hash: t.String(),
+        nombre: t.String(),
+        avatar_url: t.Optional(t.String()),
+      }),
+    }
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
